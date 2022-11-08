18d1c47b1328a7e062a68dec6a254d757bbba39e
Dulce Palacios Aristondo <dpalacios@strava.com>
2022-11-07 17:14:34 -0800

    Rename target

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "TestFiles",
-            targets: ["json"]
+            targets: ["TestFiles"]
         ),
     ],
     dependencies: [
@@ -20,7 +20,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "json",
+            name: "TestFiles",
             dependencies: [],
             path: "../test-files",
             resources: [.copy("json/group1"), .copy("json/group2"), .copy("README.md")]
